c34715d5d5af62b5a522336a59484e351c53446d
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-03-20 16:27:47 -0300

    Update code.py

--- code.py
+++ code.py
@@ -10,10 +10,8 @@ def insert_postgress():
     conn.autocommit = True
 
 # Preparing SQL queries to INSERT a record into the database.
-    query = "INSERT INTO track (id, data, datasys, longitude, latitude, odometro, velocidade,ignicao, rpm, transparent_data, ibutton, dir, in1, out1, out2,  gps, gprs, horimetro, voltagem, temperatura, contador, motivo, package_) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-    data = (unique_id, gps_utc_time, datasys, longitude, latitude, odometro, speed, ignicao, rpm,
-     transparent_data, rfid, azimuth, in1, out1, out2, gps_accuracy, gprs, horimetro,
-      external_power_supply_voltage, temp, count_number, motivo, package_)
+    query = "INSERT INTO track (id, value1, value2) VALUES (%s, %s, %s);"
+    data = (id, value1, value2)
 
     cursor = conn.cursor()
     cursor.execute(query, data)
