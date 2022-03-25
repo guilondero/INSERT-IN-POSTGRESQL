df781c7147df616838953cff23f832f5a198ca03
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-03-25 18:05:09 -0300

    Update code.py

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ def insert_postgress(id, value1, value2):
     conn.autocommit = True
 
 # Preparing SQL queries to INSERT a record into the database.
-    query = "INSERT INTO track (id, value1, value2) VALUES (%s, %s, %s);"
+    query = "INSERT INTO "YOURTABLE" (id, value1, value2) VALUES (%s, %s, %s);"
     data = (id, value1, value2)
 
     
